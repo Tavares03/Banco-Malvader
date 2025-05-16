6bd47e5e0e5ba24006fa818028ddb57cdf06f58e
Gabriel Tavares <gabriel.tbm16@gmail.com>
2025-05-16 14:01:19 -0300

    att

--- banco-malvader/app.py
+++ banco-malvader/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from routes.login_route import login_route
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 app.register_blueprint(login_route)
 
 if __name__ == '__main__':
